2346d59c225e684919998bdccdee921bc05c2a62
tikkhun <s565338107@163.com>
2024-08-29 22:57:10 +0800

    chore: sash

--- packages/react-ui/src/App.tsx
+++ packages/react-ui/src/App.tsx
@@ -1,26 +1,32 @@
-/**
- * @author
- * @file App.tsx
- * @fileBase App
- * @path packages\react-ui\src\App.tsx
- * @from
- * @desc
- * @todo
- *
- *
- * @done
- * @example
- */
 import React from "react";
-import { ThreeColumnLayout, MaxHeight, TheTime } from "../lib";
-function App() {
+import Sash from "../lib/layout/Sash";
+
+const App: React.FC = () => {
   return (
-    <ThreeColumnLayout className="w-20 h-screen">
-      <div className="h-screen ">Left Panel Content</div>
-      <div className="h-screen ">Middle Panel Content</div>
-      <div className="h-screen ">Right Panel Content</div>
-    </ThreeColumnLayout>
+    <div className="w-screen h-screen">
+      <Sash
+        orientation="vertical"
+        defaultSize={300}
+        minSize={200}
+        maxSize={500}
+      >
+        <div className="p-4 bg-blue-100">
+          <h2 className="mb-4 text-xl font-bold">Left Panel</h2>
+          <p>
+            This is the content of the left panel. You can resize it by dragging
+            the sash.
+          </p>
+        </div>
+        <div className="p-4 bg-green-100">
+          <h2 className="mb-4 text-xl font-bold">Right Panel</h2>
+          <p>
+            This is the content of the right panel. It will adjust its size as
+            you resize the left panel.
+          </p>
+        </div>
+      </Sash>
+    </div>
   );
-}
+};
 
 export default App;
