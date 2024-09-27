4983827aab6d78f0608f74f0bf7d33775e0ca6be
tikkhun <s565338107@163.com>
2024-09-27 11:32:17 +0800

    chore: 状态豆。

--- packages/react-ui/lib/status-dot/status-dot.tsx
+++ packages/react-ui/lib/status-dot/status-dot.tsx
@@ -9,31 +9,41 @@
  */
 import { motion, AnimatePresence } from "framer-motion";
 import { useMemo } from "react";
+
 export enum Status {
   success = "success",
   warning = "warning",
   fail = "fail",
 }
+
 interface StatusDotProps {
   status: Status;
 }
+
 export const StatusDot: React.FC<StatusDotProps> = ({ status }) => {
+  const dotColor = useMemo(() => {
+    switch (status) {
+      case Status.success:
+        return "bg-green-500";
+      case Status.fail:
+        return "bg-red-500";
+      case Status.warning:
+        return "bg-orange-500";
+      default:
+        return "";
+    }
+  }, [status]);
+
   return (
-    // <AnimatePresence>
-    //   <motion.div
-    //     initial={{ opacity: 0 }}
-    //     animate={{ opacity: 1 }}
-    //     exit={{ opacity: 0 }}
-    //     transition={{ duration: 0.3, easing: "easeInOut" }}
-    //   >
-        <div
-          className={`w-2 h-2 rounded-full 
-        ${status === Status.success && "bg-green-500"}
-        ${status === Status.fail && "bg-red-500"}
-        ${status === Status.warning && "bg-orange-500"}
-        `}
-        ></div>
-    //   </motion.div>
-    // </AnimatePresence>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={status}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.3 }}
+        className={`w-2 h-2 rounded-full ${dotColor}`}
+      ></motion.div>
+    </AnimatePresence>
   );
 };
