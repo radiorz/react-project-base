cb9f5458f9acc56478e86e8edea80e0951f95a5d
tikkhun <s565338107@163.com>
2025-04-02 10:17:36 +0800

    chore:ui-shadcn 用来扩充 shadcn的组件库

---
+++ packages/ui-shadcn/example/App.tsx
@@ -0,0 +1,22 @@
+/**
+ * @author
+ * @file App.tsx
+ * @fileBase App
+ * @path packages\ui-shadcn\example\App.tsx
+ * @from
+ * @desc
+ * @example
+ */
+
+import { Button } from "../src/components/ui/button";
+import "../src/index.css";
+export const App: React.FC = () => {
+  return (
+    <div>
+      <Button> 123</Button>
+    </div>
+  );
+};
+
+// 默认导出
+export default App;
