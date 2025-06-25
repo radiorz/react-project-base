274b598d8562f5c20edc39d23d66ba3df85e8c05
tikkhun <s565338107@163.com>
2025-06-25 15:14:28 +0800

    chore: update.

--- projects/react19-template/src/App.tsx
+++ projects/react19-template/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+ * @author
+ * @file App.tsx
+ * @fileBase App
+ * @path projects\react19-template\src\App.tsx
+ * @from
+ * @desc
+ * @example
+ */
 
-function App() {
-  const [count, setCount] = useState(0)
+import { createAlova } from 'alova';
+import { useRequest } from 'alova/client';
+import ReactHook from 'alova/react';
+import adapterFetch from 'alova/fetch';
 
+const alova = createAlova({
+  baseURL: 'http://192.168.111.172:9000',
+  statesHook: ReactHook,
+  requestAdapter: adapterFetch(),
+  responded: (response) => {
+    return response.json();
+  },
+});
+export const App: React.FC /* <AppProps> */ = () => {
+  const { data, loading, error, send } = useRequest(
+    () => alova.Post('/login', { username: 'admin', password: '111111' }),
+    {
+      immediate: false,
+      force: true,
+    },
+  );
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button onClick={() => send(true)}>123123123123</button>
+      {loading ?? 'loading'}
+      {error ? error?.message : ''}
+      <textarea>{JSON.stringify(data, null, 2)}</textarea>
+    </div>
+  );
+};
 
-export default App
+// 默认导出
+export default App;
